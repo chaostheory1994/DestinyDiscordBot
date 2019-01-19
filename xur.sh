2c156e74672075f0564a50399355edf1a3edbe07
Jacob Schreiner <jmschreiner2@gmail.com>
2019-01-19 16:12:02 -0500

    added the scripts to download and post weekly_reset and xur wares png

---
+++ xur.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OUTPUT_DIR="/tmp"
+WEBHOOK_URL=""
+
+FILE_URL="$(curl https://www.niris.tv/blog/xurs-wares | grep '<img.*xur.* />' | grep -Po ' src="\K.*?(?=")' | sed 's/\?.*//')"
+OUTPUT_FILE="$OUTPUT_DIR$(echo $FILE_URL | grep -Po '/[^/]*\.[a-z]*$')"
+
+echo Found: $FILE_URL
+echo Saving to: $OUTPUT_FILE
+
+if [ -f $OUTPUT_FILE ]; then
+	echo $OUTPUT_FILE already exists.
+else
+	echo Downloading
+	wget --output-document="$OUTPUT_FILE" $FILE_URL
+
+	if [ -f $OUTPUT_FILE ]; then
+		echo Sending $OUTPUT_FILE to discord
+		curl -F "weekly-reset=@$OUTPUT_FILE" $WEBHOOK_URL
+	else
+		echo Something went wrong. Did not download picture.
+	fi
+fi
